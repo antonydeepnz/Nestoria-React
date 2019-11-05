0c9406294f0aca6622ac50335e7b51766ce3947d
antonydeepnz <mashoshini@gmail.com>
2019-11-05 11:52:11 +0300

    3 files modified

--- store/actions/listingsActions.js
+++ store/actions/listingsActions.js
@@ -1,15 +1,15 @@
 import axios from 'axios';
 
-import { requestData, requestDataFulfilled, requestDataRejected } from '../reducers/listings'
+import { getListingsRequest, getListingsFulfiled, getListingsRejected } from '../reducers/listings'
 
 
 export const getListings = (town) => {
   return dispatch => {
-    dispatch(requestData(true));
+    dispatch(getListingsRequest(true));
     axios.get(`https://cors-anywhere.herokuapp.com/https://api.nestoria.co.uk/api?encoding=json&pretty=1&action=search_listings&country=uk&listing_type=rent&place_name=${town}`)
     .then(res => {
-      dispatch(requestDataFulfilled(res.data.response.listings));
+      dispatch(getListingsFulfiled(res.data.response.listings));
     })
-    .catch(err => dispatch(requestDataRejected(err)));
+    .catch(err => dispatch(getListingsRejected(err)));
   }
 }
--- store/reducers/listings.js
+++ store/reducers/listings.js
@@ -1,3 +1,5 @@
+import { createActions, handleActions } from 'redux-actions';
+
 const initialState = () => {
   return {
     listings: [],
@@ -7,41 +9,58 @@ const initialState = () => {
   }
 } 
 
-export function listings(state = initialState(), action){
-  switch (action.type){
-    case "GET_LISTINGS": {
-      return {...state, loading: action.payload};
-    }
-    case "GET_LISTINGS_FULFILED": {
-      return {...state, listings: action.payload, loading: action.loading};
-    }
-    case "GET_LISTINGS_REJECTED": {
-      return {...state, errorMessage: action.payload, loading: action.loading};
-    } 
-    default: 
-      return state
-  }
-}
+export const { getListingsRequest, getListingsFulfiled, getListingsRejected } = createActions(
+  
+    "GET_LISTINGS_REQUEST",
+    'GET_LISTINGS_FULFILED',
+    'GET_LISTINGS_REJECTED' 
+) 
 
-export const requestData = (bool) => {
-    return {
-        type: "GET_LISTINGS",
-        payload: bool,
-    };
-}
 
-export const requestDataFulfilled = (data) => {
-    return {
-        type: "GET_LISTINGS_FULFILED",
-        payload: data,
-        loading: false,
-    };
-}
+// export const requestData = (state, bool) => {
+//     return {...state, loading: bool};
+// }
 
-export const requestDataRejected = (error) => {
-    return {
-        type: "GET_LISTINGS_REJECTED",
-        payload: error,
-        loading: false,
-    };
-}
+// export const requestDataFulfilled = (data) => {
+//     return {
+//         type: "GET_LISTINGS_FULFILED",
+//         payload: data,
+//         loading: false,
+//     };
+// }
+
+// export const requestDataRejected = (error) => {
+//     return {
+//         type: "GET_LISTINGS_REJECTED",
+//         payload: error,
+//         loading: false,
+//     };
+// }
+
+// export function listings(state = initialState(), action){
+//   switch (action.type){
+//     // case "GET_LISTINGS": {
+//     //   return {...state, loading: action.payload};
+//     // }
+//     case "GET_LISTINGS_FULFILED": {
+//       return {...state, listings: action.payload, loading: action.loading};
+//     }
+//     case "GET_LISTINGS_REJECTED": {
+//       return {...state, errorMessage: action.payload, loading: action.loading};
+//     } 
+//     default: 
+//       return state
+//   }
+// }
+
+export const listings = handleActions(
+  {
+    [getListingsRequest]: (state,bool) => ({...state, loading: bool}),
+  
+    [getListingsFulfiled]: (state,data) => ({...state, listings: data, loading: false})
+,
+  
+    [getListingsRejected]: (state,error) => ({...state, errorMessage: error, loading: false})
+  },  
+  initialState()
+);
