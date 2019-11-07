c8a2f00ddf71c1f20d8502794ac0f127e92cae5c
antonydeepnz <mashoshini@gmail.com>
2019-11-07 15:26:49 +0300

    1 file modified

--- components/Listing.js
+++ components/Listing.js
@@ -11,7 +11,7 @@ export const Listing = (props) => {
       <img src={props.imgSrc} />
       <h4>{props.title}</h4>
       <p>{props.listing_type === 'rent'? 
-          `Rent for ${props.price_formatted} `:
+          `${props.price_formatted} ${price_type}`:
           `Buy for ${props.price_formatted} ${price_type}`}</p>
       <a className='listing-getmore-btn'>More Info</a>
       <SaveToFavorites data={props.data} checked={props.checked}/>
